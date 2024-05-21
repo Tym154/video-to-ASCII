37c3a9c13636b807890a6327c29c80f7df87186b
Tym154 <159148113+Tym154@users.noreply.github.com>
2024-05-21 22:29:49 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,23 @@
-#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/opencv.hpp>
-#include <stdio.h>
 #include <iostream>
-
-using namespace cv;
-using namespace std;
 
 
 int main(){
-    cout << CV_VERSION;
+    cv::Mat frame = cv::imread("image.png");
+    cv::namedWindow("Output", 0);
+    cv::namedWindow("Input", 0);
 
+    cv::Mat output;
+    
+    cv::Mat resized_image(frame.rows *0.5, frame.cols * 0.5, CV_8UC3);
+
+    cv::resize(frame, resized_image, cv::Size(), 0.5, 0.5);
+
+    cv::imshow("Output", output);
+    cv::imshow("Input", frame);
+    
+    cv::waitKey(0);
     return 0;
 }
