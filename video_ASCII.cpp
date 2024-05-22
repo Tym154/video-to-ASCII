e0ef201b016ff341231e2d3595129a33a90d574f
Tym154 <159148113+Tym154@users.noreply.github.com>
2024-05-22 15:35:08 +0200

    Create video_ASCII.cpp

---
+++ video_ASCII.cpp
@@ -0,0 +1,46 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <thread>
+
+using namespace cv;
+using namespace std;
+
+
+int main(){
+    string ascii = "   .!/?&W#@M";
+    string video_path = "video2.mp4";
+    VideoCapture cap(video_path);
+
+    double fps = cap.get(CAP_PROP_FPS);
+
+    int frame_duration = 1000 / fps;
+
+    Mat frame, resized_frame;
+    while(1){
+
+        cap >> frame;  
+
+        resize(frame, resized_frame, Size(), 0.2, 0.2, INTER_CUBIC);
+
+        string frame_ascii = "";
+
+        for(int i = 0; i < resized_frame.rows; i++){
+            for(int j = 0; j < resized_frame.cols; j++){
+                frame_ascii += ascii[floor(((resized_frame.at<Vec3b>(i,j)[0]+resized_frame.at<Vec3b>(i,j)[1]+resized_frame.at<Vec3b>(i,j)[2])/3)/(255/ascii.length()))];
+            }
+            frame_ascii += "\n";   
+        }
+
+        system("clear");
+        std::cout << frame_ascii;
+        std::this_thread::sleep_for(std::chrono::milliseconds(frame_duration));
+
+    }
+
+    return 0;
+}
