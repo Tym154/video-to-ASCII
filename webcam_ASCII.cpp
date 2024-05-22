0790f80d35c7a7abf64d0eafa047d2503072d566
Tym154 <159148113+Tym154@users.noreply.github.com>
2024-05-22 15:36:00 +0200

    Update and rename main.cpp to webcam_ASCII.cpp

--- webcam_ASCII.cpp
+++ webcam_ASCII.cpp
@@ -12,35 +12,34 @@ using namespace std;
 
 
 int main(){
-    string ascii = "   .!/?&W#@M";
-    string video_path = "video2.mp4";
-    VideoCapture cap(video_path);
+    string ascii = " .!/?&W#@M";
+    VideoCapture cap(0);
 
     double fps = cap.get(CAP_PROP_FPS);
 
     int frame_duration = 1000 / fps;
 
     Mat frame, resized_frame;
-
-
     while(1){
+
         cap >> frame;  
 
-        if(frame.empty()) cout << " ERDSADAS";
+        resize(frame, resized_frame, Size(), 0.3, 0.3, INTER_CUBIC);
 
-        resize(frame, resized_frame, Size(), 0.05, 0.05, INTER_LINEAR);
-
-        string frame_ascii;
+        string frame_ascii = "";
 
         for(int i = 0; i < resized_frame.rows; i++){
             for(int j = 0; j < resized_frame.cols; j++){
-                frame_ascii += ascii[floor(((resized_frame.at<Vec3b>(i,j)[0]+resized_frame.at<Vec3b>(i,j)[1]+resized_frame.at<Vec3b>(i,j)[2])/3)/(255/ascii.length()))];
+                frame_ascii += ascii[floor(((resized_frame.at<Vec3b>(i,j)[0]+resized_frame.at<Vec3b>(i,j)[1]+resized_frame.at<Vec3b>(i,j)[2])/3)/(255/ascii.length())-1 )];
             }
             frame_ascii += "\n";   
         }
+
         system("clear");
         std::cout << frame_ascii;
         std::this_thread::sleep_for(std::chrono::milliseconds(frame_duration));
+
     }
+
     return 0;
 }
