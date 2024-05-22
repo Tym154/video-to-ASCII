d01cfa49f0e389b2159250830ed362345fbffd74
Tym154 <159148113+Tym154@users.noreply.github.com>
2024-05-22 15:44:35 +0200

    Update video_ASCII.cpp

--- video_ASCII.cpp
+++ video_ASCII.cpp
@@ -12,9 +12,8 @@ using namespace std;
 
 
 int main(){
-    string ascii = "   .!/?&W#@M";
-    string video_path = "video2.mp4";
-    VideoCapture cap(video_path);
+    string ascii = " .!/?&W#@M";
+    VideoCapture cap("video.mp4");
 
     double fps = cap.get(CAP_PROP_FPS);
 
@@ -25,13 +24,13 @@ int main(){
 
         cap >> frame;  
 
-        resize(frame, resized_frame, Size(), 0.2, 0.2, INTER_CUBIC);
+        resize(frame, resized_frame, Size(), 0.5, 0.5, INTER_CUBIC);
 
         string frame_ascii = "";
 
         for(int i = 0; i < resized_frame.rows; i++){
             for(int j = 0; j < resized_frame.cols; j++){
-                frame_ascii += ascii[floor(((resized_frame.at<Vec3b>(i,j)[0]+resized_frame.at<Vec3b>(i,j)[1]+resized_frame.at<Vec3b>(i,j)[2])/3)/(255/ascii.length()))];
+                frame_ascii += ascii[floor(((resized_frame.at<Vec3b>(i,j)[0]+resized_frame.at<Vec3b>(i,j)[1]+resized_frame.at<Vec3b>(i,j)[2])/3)/(255/ascii.length())-1 )];
             }
             frame_ascii += "\n";   
         }
